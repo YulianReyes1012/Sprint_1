52013d86f41d8d7ac5d55919f14456f8a67f58e1
YulianReyes1012 <yreyess@HOTMAIL.COM>
2021-10-07 22:48:13 -0500

    07102021

--- js/registro.js
+++ js/registro.js
@@ -1,14 +1,13 @@
-// arreglo ( vector )
-var arreglo = [];
+var registros = [];
 
 function almacenarRegistros(){
-// variables
+
     var usuario = document.getElementById("campoNombre").value;
     var contrasena = document.getElementById("campoContrasena").value;
     var correo = document.getElementById("campoCorreo").value;
     var confcorreo = document.getElementById("campoConfirmacion").value;
     var telefono = document.getElementById("campoTelefono").value;
-// diccionario
+
     var diccionario = {
         "usuario":usuario,
         "contrasena":contrasena,
@@ -16,18 +15,23 @@ function almacenarRegistros(){
         "confcorreo":confcorreo,
         "telefono":telefono
     }
-// es menor a 30
-    if (arreglo.length<30){
-        arreglo.push(diccionario);
-// imprime
+
+    if (registros.length<30){
+        registros.push(diccionario);
+
         console.log("diccionario",diccionario);
-        console.log("arreglo",arreglo);
+        console.log("arreglo",registros);
         return true;
     }
-    console.log("arreglolleno",arreglo);
+    console.log("arreglolleno",registros);
     return false;
     
 }
 
-//module.exports.validar_correo = validar_correo;
-//module.exports.validar_contrasena_usuario = validar_contrasena_usuario;
+function ordenarRegistros(arreglo){
+    arreglo.sort();
+    return arreglo;
+}
+
+module.exports.almacenarRegistros = almacenarRegistros();
+module.exports.ordenarRegistros = ordenarRegistros();
